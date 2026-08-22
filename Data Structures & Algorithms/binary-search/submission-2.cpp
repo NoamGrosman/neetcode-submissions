58cbd2cd438c7f00c16b4c650a02cb27fb43b072
NoamGrosman <noam074@gmail.com>
2026-08-22 19:42:38 +0300

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) return m;
+            if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return -1;
+    }
+};
+
+
+// This is a very common binary search problem
+// We know the array is sorted, and the numbers are distinct
+// Solution is just implementing Binary Search.
