26342a811bd510c094b55d92d61a9246400e4dbb
NoamGrosman <noam074@gmail.com>
2026-08-31 15:23:29 +0300

    Add: validate-parentheses - submission-13

---
+++ Data Structures & Algorithms/validate-parentheses/submission-13.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> paren;
+        for (char ch : s) {
+            if (ch == '{' || ch == '(' || ch == '[') {
+                paren.push(ch);
+            }
+            else if (paren.empty()) return false;
+            else {
+                if ((ch == ')' && paren.top() == '(') || (ch == '}' && paren.top() == '{') || (ch == ']' && paren.top() == '[')) {
+                    paren.pop();
+                }
+                else {
+                    return paren.empty();
+                }
+            }
+        }
+        return paren.empty();
+    }
+};
